e92a07b9edcf23d945a2aabac722522ec8548f7b
jasonkalmeida <jasonkalmeida@gmail.com>
2015-07-18 10:40:55 -0700

    Created Fetch and Server

---
+++ fetch.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var app = express();
+var Twitter = require('twitter');
+
+
+var path = require('path');
+
+var followers;
+
+
+var fs = require('fs');
+
+var client = new Twitter({
+  consumer_key: 'xOMDbOMHe0K18MctufpiA4KW2',
+  consumer_secret: 'o6qcAKtpXQazeo37kRrCiym4z8Y3pQKoxZnp2HVuVzQFpOR70n',
+  access_token_key: '2727197461-fAW2kCQ6hoYLLEMNODbDgBVPUvWWJ0nYUv8Fd4E',
+  access_token_secret: '3pynIwkP7LHpUIkf357mtcPsIUSaouQvIBXbo52Ktbtud'
+});
+ 
+
+
+client.get('followers/list.json?count=9', function(error, response){
+  if(error) throw error;
+  followers = response;
+  
+  
+  
+  
+  var obj = [];
+  
+  for(var fcount in followers.users)
+  {
+	  var hold = 'friends/list.json?cursor=-1&screen_name=' + followers.users[fcount].screen_name + '&skip_status=true';
+  
+	 
+	  
+  	  
+  
+	  client.get(hold, function(error, res){
+		  
+		   console.log(res);
+		  
+		  var str = JSON.parse(res);
+		  
+		  str.users.forEach(function(item)
+	  {
+	  	obj.push(item.name)
+		
+	  })
+		  
+	
+		  /*for(var test in res.users) {
+			  
+		  	 obj = obj.concat()
+			  
+		     console.log("username:"+  res.users[test].name);
+		  }*/
+	
+	  });
+  }
+  
+  obj = JSON.stringify(obj);
+  
+  fs.writeFileSync(path.join(__dirname, './tweettemp.json'), obj , 'utf8')
+
+});
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var hbs = require('hbs');
 
 var path = require('path');
 
+var fs = require('fs');
+
+
 var followers;
 app.set('views', path.join(process.cwd(), './views'))
 app.set('view engine', 'hbs')
@@ -13,14 +16,6 @@ app.engine('hbs', hbs.__express)
 
 app.use(express.static(__dirname + '/public'));
 
- 
-var client = new Twitter({
-  consumer_key: 'xOMDbOMHe0K18MctufpiA4KW2',
-  consumer_secret: 'o6qcAKtpXQazeo37kRrCiym4z8Y3pQKoxZnp2HVuVzQFpOR70n',
-  access_token_key: '2727197461-fAW2kCQ6hoYLLEMNODbDgBVPUvWWJ0nYUv8Fd4E',
-  access_token_secret: '3pynIwkP7LHpUIkf357mtcPsIUSaouQvIBXbo52Ktbtud'
-});
- 
 
 app.post('/myaction', function(req, res) {
     
@@ -28,44 +23,28 @@ app.post('/myaction', function(req, res) {
 });
 
 
-client.get('followers/list.json?count=9', function(error, response){
-  if(error) throw error;
-  followers = response;
-  //console.log(followers.users[1].name);
-  
-  
-  
-  
-  for(var fcount in followers.users)
-  {
-	  var hold = 'friends/list.json?cursor=-1&screen_name=' + followers.users[fcount].screen_name + '&skip_status=true';
-  
-  
-	  client.get(hold, function(error,res){
-		  
-		  console.log(res);
-	
-		  for(var test in res.users) {
-		     console.log("username:"+  res.users[test].name);
-		  }
-	
-	  });
-  }
-
-});
 
 app.get('/', function (req, res){
+	
+	var obj = require ('./tweettemp.json');
+	
+	
 	
 	res.render('index', {
 		
 		layout:false,
-		handles: ['@jasonkalmeida', '@jsnklmd', '@twitter']
+		handles: obj
 		
 	})
 }
 
 
 )
+
+
+
+
+
 
 app.listen(process.env.PORT || 3000);
 
